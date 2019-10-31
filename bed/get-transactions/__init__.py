8091b12146b84d0fcf4720387675d38602b38e50
ana <ana.smirnova@outlook.com>
2019-10-31 23:39:15 +1100

    fix: added content type response headers

--- bed/get-transactions/__init__.py
+++ bed/get-transactions/__init__.py
@@ -6,4 +6,4 @@ import azure.functions as func
 def main(req: func.HttpRequest, transactions):
     body_dict = json.loads(transactions)
     body_json = json.dumps(body_dict)
-    return func.HttpResponse(status_code=200, body=body_json)
+    return func.HttpResponse(status_code=200,headers={'Content-Type': 'application/json'}, body=body_json)
